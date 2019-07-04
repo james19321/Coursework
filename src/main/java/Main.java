e58074533b6de4de045d95e658545d7070552412
james19321 <87946@farnborough.ac.uk>
2019-07-04 10:29:34 +0100

    Database can now be opened

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import org.sqlite.SQLiteConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        openDatabase("Library.db");
+    }
+
+    public static Connection db = null;
+
+    private static void openDatabase(String dbFile){
+
+        try{
+
+            Class.forName("org.sqlite.JDBC");
+            SQLiteConfig config = new SQLiteConfig();
+            config.enforceForeignKeys(true);
+            db = DriverManager.getConnection("jdbc:sqlite:resources/"+dbFile, config.toProperties());
+            System.out.println("Database connection successfully established");
+        }catch (Exception exception){
+            System.out.println("Database connection error: " + exception.getMessage());
+        }
     }
 }
